d30ba823ba447afea4fa117eeae66c794eff50b0
Ragabharathi <84565134+Ragabharathi@users.noreply.github.com>
2021-11-23 10:15:07 +0530

    Create Unique Binary Search Tree Catalan Num.c

---
+++ Unique Binary Search Tree Catalan Num.c
@@ -0,0 +1,18 @@
+Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+Example 1:
+Input: n = 3
+Output: 5
+  
+Example 2:
+Input: n = 1
+Output: 1
+
+
+
+int numTrees(int n){
+ long C = 1;
+    for(int i = 0; i < n; ++i) {
+        C = C * 2 * (2 * i + 1) / (i + 2);
+    }
+    return (int) C;
+}
